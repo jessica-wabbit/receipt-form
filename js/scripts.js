bde4218c1dfbc7cbe32807e79ae5f427340e2242
Jessica Robertson and Susha Dore <susha@>
2017-02-13 20:45:29 -0800

    Add rows and columns with divs to style both HTML pages.

--- js/scripts.js
+++ js/scripts.js
@@ -21,5 +21,22 @@ $(function() {
 
   });
 
+  $("#survey form").submit(function(event) {
+
+    var scent = $("#scent").val();
+    var color = $("input:radio[name=color]:checked").val();
+    var dob = $("#born").val();
+
+
+    $(".scent").text(scent);
+    $(".color").text(color);
+    $(".dob").text(dob);
+
+    $("#survey-results").show();
+
+    event.preventDefault();
+
+  });
+
 
 });
