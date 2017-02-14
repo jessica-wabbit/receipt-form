fd8e3d397ada60356773747fb31faac5d0b8df50
Jessica Robertson and Susha Dore <susha@>
2017-02-13 20:13:16 -0800

    Add calendar to mystery-box.html using jquery and link both HTML pages to each other.

--- js/mystery-scripts.js
+++ js/mystery-scripts.js
@@ -3,10 +3,13 @@ $(function() {
 
       var scent = $("#scent").val();
       var color = $("input:radio[name=color]:checked").val();
+      var dob = $("#born").val();
 
 
       $(".scent").text(scent);
       $(".color").text(color);
+      $(".dob").text(dob);
+
       $("#survey-results").show();
 
       event.preventDefault();
