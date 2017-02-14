c52fb86ca3ed8a5f101bab3332d5d67164bb4404
Jessica Robertson and Susha Dore <jessica@>
2017-02-13 19:59:47 -0800

    Add radio button for survey color preference

--- js/mystery-scripts.js
+++ js/mystery-scripts.js
@@ -2,9 +2,11 @@ $(function() {
     $("#survey form").submit(function(event) {
 
       var scent = $("#scent").val();
+      var color = $("input:radio[name=color]:checked").val();
+
 
       $(".scent").text(scent);
-
+      $(".color").text(color);
       $("#survey-results").show();
 
       event.preventDefault();
